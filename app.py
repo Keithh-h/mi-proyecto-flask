4c85ccb5d8ebce1e28216fcde7cb9c5ac2abbc51
Keithh-h <ykvargasvillacrez@gmail.com>
2026-06-02 15:00:48 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-# Importamos Flask y la función render_template para poder mostrar archivos HTML
 from flask import Flask, render_template
 
-# Creamos la aplicación de Flask y la guardamos en una variable llamada 'app'
 app = Flask(__name__)
 
-# Definimos qué pasará cuando alguien entre a la página principal (la raíz '/')
+# 👇 ESTA LÍNEA ES CRUCIAL PARA VERCEL: Le dice explícitamente qué usar
+app.index = app 
+
 @app.route('/')
 def inicio():
-    # Le decimos a Flask que busque el archivo 'index.html' dentro de la carpeta 'templates' y lo muestre
     return render_template('index.html')
 
 if __name__ == '__main__':
